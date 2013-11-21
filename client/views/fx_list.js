c7c2fe3a0c04500c73f9392625779210729c0d48
Abderrazak <bouadma.abderrazak@gmail.com>
2013-11-22 00:58:24 +0100

    adding templates addinf data

---
+++ client/views/fx_list.js
@@ -0,0 +1,33 @@
+/*
+var fxData = [
+    {
+        "location": "Paris",
+        "currency": "1 EUR",
+        "target": "USD",
+        "country":"us",
+        "rate":1.35},
+    {
+        "location": "Paris",
+        "currency": "1 EUR",
+        "target": "CHF",
+        "country":"cg",
+        "rate":1.33},
+    {
+        "location": "Paris",
+        "currency": "1 EUR",
+        "target": "CAN",
+        "country":"ca",
+        "rate":1.42},
+    {
+        "location": "Paris",
+        "currency": "1 EUR",
+        "target": "YEN",
+        "country":"jp",
+        "rate":136.08
+    }
+];
+
+*/
+Template.fxList.helpers({
+    fx: null
+});
