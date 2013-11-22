d7c00a0a0d67da79ab0e3bc1617978b194614d7d
Abderrazak <bouadma.abderrazak@gmail.com>
2013-11-22 11:38:49 +0100

    Accounts

---
+++ client/helpers/config.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: abderrazak
+ * Date: 11/22/13
+ * Time: 11:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+/*
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
+*/
