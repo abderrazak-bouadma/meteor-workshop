77bce12e28aa3f50a4432f28ba1d95e3c31841d8
Abderrazak <bouadma.abderrazak@gmail.com>
2013-11-22 01:29:53 +0100

    first use of collections

---
+++ collections/fx.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: abderrazak
+ * Date: 11/22/13
+ * Time: 1:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+ForeignX = new Meteor.Collection("foreignX");
---
+++ server/fixtures.js
@@ -0,0 +1,39 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: abderrazak
+ * Date: 11/22/13
+ * Time: 1:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+/*
+if (ForeignX.find().count() === 0) {
+
+    ForeignX.insert({
+        "location":"France",
+        "currency":"EUR",
+        "target":"USD",
+        "country":"us",
+        "rate":1.35});
+
+    ForeignX.insert({
+        "location":"France",
+        "currency":"EUR",
+        "target":"CHF",
+        "country":"ch",
+        "rate":1.33});
+
+    ForeignX.insert({
+        "location":"France",
+        "currency":"EUR",
+        "target":"CAN",
+        "country":"ca",
+        "rate":1.42});
+
+    ForeignX.insert({
+        "location":"France",
+        "currency":"EUR",
+        "target":"YEN",
+        "country":"jp",
+        "rate":136.08});
+}
+*/
